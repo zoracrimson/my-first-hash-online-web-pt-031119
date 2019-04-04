542505a435fdccc905a95cebb3c0d19fc71d5718
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 02:13:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,14 +23,16 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 shipping_manifest["oil paintings"] 
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"] = 2 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
+shipping_manifest
 
 end
