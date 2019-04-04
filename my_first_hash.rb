3f2577289be05e824d2b7115d9a932c786188598
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 02:03:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,10 @@
 def my_hash
   my_hash = {"puppy_stamps" => 4}
- equal to a hash with key/value pairs of your choice.
-
-  
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
